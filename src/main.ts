0ecb1bf2866c483e037ef7290fed4b09a45a1d8b
bpajor <pejdzor@student.agh.edu.pl>
2024-01-01 14:42:34 +0100

    make it better

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder().setTitle('Poland Electionsc 2023 API')
+  .setTitle('Poland Elections 2023 API')
+  .setDescription('The Poland Elections 2023 API description')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
